6eb5784fba27ea55bf23b9d5d45a3dee20f17744
Nikita Shchebelenkov <nschebelenkov@me.com>
2023-12-27 15:37:51 +0400

    hw2. test commit.

---
+++ SearchTest.java
@@ -0,0 +1,9 @@
+public class SearchTest {
+
+    @Test
+    void successfulSearchTest() {
+        open("https://www.google.com/");
+        $("[name=q]").setValue("selenide").pressEnter();
+        $("[id=search]").shouldHave(text("https://selenide.org"));
+    }
+}
